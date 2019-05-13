f8f3551c3c51eabe57eb4ba8f59f328b718aacb6
Gór Milán <milan.projekt@server.devop.hu>
2019-05-13 20:14:17 +0200

    authentication, and users

---
+++ source_code/basic/messages/hu-HU/app.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    'Site index' => 'Kezdőoldal',
+    'Date' => 'Dátum',
+    'Create {modelClass}' => 'Új {modelClass}',
+    'Are you sure you want to delete this item?' => 'Biztos törli ezt az elemet?',
+    'Create' => 'Létrehozás',
+    'Delete' => 'Törlés',
+    'Reset' => 'Alaphelyzet',
+    'Search' => 'Keres',
+    'Update' => 'Módosítás',
+    'Update {modelClass}: ' => '{modelClass} módosítás: ',
+    'Save' => 'Mentés',
+    'City' => 'Város',
+    'Created Date' => 'Létrehozva',
+    'Modified At' => 'Modified At',
+    'Name' => 'Név',
+    'Start' => 'Kezdés',
+    'Finish' => 'Befejezés',
+    'Sum' => 'Összesen',
+    'Used' => 'Használatban',
+    'Back' => 'Vissza',
+    'Cancel' => 'Mégsem',
+    'User' => 'Felhasználó',
+    'print' => 'Nyomtatás',
+    'All' => 'Mind'
+ ];  
+
+
