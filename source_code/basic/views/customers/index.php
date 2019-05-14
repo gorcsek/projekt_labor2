27160b7e2b5c3bf9e8b4abcb974ecbafe06323f8
Nagy Annamária <annamaria.nagy06@gmail.com>
2019-05-14 20:51:04 +0200

    Nagy Annamária 2019.05.14 Customer index hozzáadása

---
+++ source_code/basic/views/customers/index.php
@@ -0,0 +1,44 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+use yii\widgets\Pjax;
+/* @var $this yii\web\View */
+/* @var $searchModel app\models\CustomersSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = Yii::t('app', 'Customers');
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="customers-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a(Yii::t('app', 'Create Customers'), ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php Pjax::begin(); ?>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'name',
+            'filling_name',
+            /*'desc:ntext',
+            'result',*/
+            'created',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+
+
+    <?php Pjax::end(); ?>
+
+</div>
