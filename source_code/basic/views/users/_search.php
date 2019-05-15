ec7f61b22fa283d964f4b8be12345807a92e376c
Nagy Annamária <annamaria.nagy06@gmail.com>
2019-05-15 20:36:05 +0200

    Nagy Annamária 2019.05.15. users/_search.php

---
+++ source_code/basic/views/users/_search.php
@@ -0,0 +1,39 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\UsersSearch */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="users-search">
+
+    <?php $form = ActiveForm::begin([
+        'action' => ['index'],
+        'method' => 'get',
+    ]); ?>
+
+    <?= $form->field($model, 'id') ?>
+
+    <?= $form->field($model, 'username') ?>
+
+    <?= $form->field($model, 'password') ?>
+
+    <?= $form->field($model, 'email') ?>
+
+    <?= $form->field($model, 'realname') ?>
+
+    <?php // echo $form->field($model, 'enabled') ?>
+
+    <?php // echo $form->field($model, 'created') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton(Yii::t('app', 'Search'), ['class' => 'btn btn-primary']) ?>
+        <?= Html::resetButton(Yii::t('app', 'Reset'), ['class' => 'btn btn-default']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
