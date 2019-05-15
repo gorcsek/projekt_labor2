2ba6a4fcee8aa91136357f33dc4863aa3bad7d23
gorcsek <gorcsek@hotmail.com>
2019-05-15 23:37:14 +0200

    update user view

--- source_code/basic/views/users/view.php
+++ source_code/basic/views/users/view.php
@@ -33,7 +33,7 @@ $this->params['breadcrumbs'][] = $this->title;
             //'password',
             'email:email',
             'realname',
-            'rfid',
+            //'rfid',
             'enabled',
             'created',
         ],
