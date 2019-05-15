c95b28d7d3254f40715ac6ee21a31b2c85c8241a
Nagy Annamária <annamaria.nagy06@gmail.com>
2019-05-15 20:33:36 +0200

    Nagy Annamária 2019.05.15. users/_form.php

---
+++ source_code/basic/views/users/_form.php
@@ -0,0 +1,58 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+use yii\web\View;
+/* @var $this yii\web\View */
+/* @var $model app\models\Users */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="users-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'username')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'realname')->textInput(['maxlength' => true]) ?>
+    
+    <?= $form->field($model, 'rfid')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'password')->passwordInput(['maxlength' => true]) ?>
+
+    <?= Html::label(Yii::t('app', 'Password retry'), 'password-retry', ['class' => 'control-label pass' ])?>
+    
+    <?= Html::input('password', 'password-retry', '', ['class' => 'form-control pass' ,'id' => 'password-retry']) ?>
+    <br>
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Update'), ['id' => 'submitButton', 'class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
+<?php
+$this->registerJs(
+    "
+    $(document).ready(function(){
+        $('#submitButton').attr('disabled',true);
+        chkPass();
+        $('.pass').change(function(){
+            chkPass();
+        })
+    })
+    function chkPass(){
+        if($('#users-password').val() == $('#password-retry').val()){
+            $('#submitButton').removeAttr('disabled');
+        }else{
+            $('#submitButton').attr('disabled',true);
+        }
+    }
+    ",
+     View::POS_READY,
+    'my-button-handler'
+);
+
+?>
