f753c02a7ccb0702e6befbc7d8912a879bd1959c
Projektlabor2 <projekt@server.devop.hu>
2019-05-16 22:28:09 +0200

    update deploy

--- source_code/basic/web/deploy.php
+++ source_code/basic/web/deploy.php
@@ -19,7 +19,7 @@ echo <<<EOT
 EOT;
 // Check whether client is allowed to trigger an update
 $allowed_ips = array(
-	'207.97.227.', '50.57.128.', '108.171.174.', '50.57.231.', '204.232.175.', '192.30.252.', // GitHub
+	'35.231.145.151','207.97.227.', '50.57.128.', '108.171.174.', '50.57.231.', '204.232.175.', '192.30.252.', // GitHub
 	'195.37.139.','193.174.' // VZG
 );
 $allowed = false;
@@ -36,7 +36,7 @@ foreach ($allowed_ips as $allow) {
         break;
     }
 }
-if ($allowed) {
+if (!$allowed) {
 	header('HTTP/1.1 403 Forbidden');
  	echo "<span style=\"color: #ff0000\">Sorry, no hamster - better convince your parents!</span>\n";
     echo "</pre>\n</body>\n</html>";
